3e0e13c2e7a75b0d747b653adaf3a13e59efdfe4
zhxh <zhxhcoder@gmail.com>
2019-07-01 09:54:16 +0800

    CWebView

---
+++ cweblayout/src/main/java/com/creditease/cweblayout/CWebView.java
@@ -0,0 +1,23 @@
+package com.creditease.cweblayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+
+/**
+ * Created by zhxh on 2019/07/01
+ */
+public class CWebView extends WebView {
+    public CWebView(Context context) {
+        super(context);
+    }
+
+    public CWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CWebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
