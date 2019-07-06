ad5d725cb8502b3335bf1733c6e223ed2cc3ce56
zhxh <zhxhcoder@gmail.com>
2019-07-06 09:50:41 +0800

    CWebView开发-BridgeHandler

---
+++ cweblayout/src/main/java/com/creditease/cweblayout/jsbridge/DefaultHandler.java
@@ -0,0 +1,11 @@
+package com.creditease.cweblayout.jsbridge;
+
+/**
+ * Created by zhxh on 2019/07/06
+ */
+public class DefaultHandler implements BridgeHandler {
+    @Override
+    public void handler(String data, CallBackFunction function) {
+
+    }
+}
